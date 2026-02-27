3461a358102b762c76d50300c65ccfef7ea24caa
felipaobagual <felipefernandegilioli@gmail.com>
2026-02-27 11:47:57 -0300

    Tipo de Dados em JS

---
+++ inicio/dados2.js
@@ -0,0 +1,10 @@
+//console.log(19.9 * 0.6);
+
+var preço = 19.9;
+var desconto = 0.6;
+var preçoComDesconto = preço * (1 - desconto);
+//console.log(preçoComDesconto);
+
+let nome = "Caderno";
+let categoria = "Papelaria";
+console.log("Produto: " + nome + ", Categoria: " + categoria);
---
+++ inicio/dados3.js
@@ -0,0 +1,14 @@
+let idade = 16;
+console.log(typeof idade);
+console.log(typeof 16);
+
+let valor = 1500.50;
+console.log(typeof valor);
+
+let estaChovendo = false; // true
+console.log(typeof estaChovendo);
+
+let nome = "Felipe";
+console.log(typeof nome);
+
+console.log(1 + 1);
