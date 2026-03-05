ae378797795f3582d15739d0789e048850819186
felipaobagual <felipefernandegilioli@gmail.com>
2026-03-05 12:12:39 -0300

    Operadores Aritimeticos

---
+++ operadores/aritimeticos.js
@@ -0,0 +1,17 @@
+// vlores literais
+console.log(10 + 5); //adição
+console.log(10 - 5); //subtração
+console.log(10 * 5); //multipicação
+console.log(10 / 5); //divisão
+console.log(10 % 5); //resto d divisão
+
+// valores variaveis
+
+let a = 100;
+let b = 50;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
