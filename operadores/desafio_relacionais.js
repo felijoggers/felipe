8089100289e8c3dc85b13f365771567a4021d223
felipaobagual <felipefernandegilioli@gmail.com>
2026-03-06 11:39:56 -0300

    operadores de atribuição e relacionais

---
+++ operadores/desafio_relacionais.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 11;
+
+//imprimir no console 5 numeros verdadeires(true)
+console.log(a == a);
+console.log(a < b);
+console.log(a != b);
+console.log(b > a);
+console.log(b === b);
+
+//imprimir no console 5 numeros verdadeires(true)
+console.log(a == b);
+console.log(a === b);
+console.log(a > b);
+console.log(b < a);
+console.log(b >= b);
